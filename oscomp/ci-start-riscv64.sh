3d52ce506689423bb1a3a021057257bd2018addb
Samuka007 <samuka007@dragonos.org>
2025-03-17 07:17:29 +0000

    feat(ide): support for vscode debuging, using lldb plugin

--- oscomp/ci-start-riscv64.sh
+++ oscomp/ci-start-riscv64.sh
@@ -3,4 +3,4 @@ qemu-system-riscv64 -machine virt -kernel ../tools/arch/riscv64/u-boot-v2023.10-
                     -no-reboot -device virtio-net-device,netdev=net -netdev user,id=net \
                     -rtc base=utc \
                     -drive file=../bin/riscv64/disk.img,if=none,format=raw,id=x1 \
-                    -device virtio-blk-device,drive=x1,bus=virtio-mmio-bus.1
+                    -device virtio-blk-device,drive=x1,bus=virtio-mmio-bus.1 -s
